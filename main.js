a831c6a57cad12df3100a4e2b150c204a801040b
hiep.vo <vanhiep1086@gmail.com>
2016-10-10 20:34:38 -0500

    add javascript & update design

--- main.js
+++ main.js
@@ -16,7 +16,11 @@
           break;
         case "del":
           if(tempVal.length > 1){
-            tempVal = tempVal.slice(0, -1);
+            if(tempVal.length === 8 && tempVal.indexOf("&minus;") === 0){ // tempVal.length = 8 = &minus;8  = -8
+              tempVal = "";
+            }
+            else
+              tempVal = tempVal.slice(0, -1);
           }
           else{
             tempVal = "";
@@ -24,24 +28,29 @@
           showData(tempVal);
           break;
         case "neg":
-          if(tempVal.indexOf("&minus;") === -1){
-            tempVal = "&minus;" + tempVal;
+          if(tempVal !== ""){
+            if(tempVal.indexOf("&minus;") === -1){
+              tempVal = "&minus;" + tempVal;
 
-          }
-          else{
-            tempVal = tempVal.replace("&minus;", "")
+            }
+            else{
+              tempVal = tempVal.replace("&minus;", "");
+            }
           }
           showData(tempVal);
           break;
         default:
           tempVal += buttonId;
+          if(tempVal.indexOf('div') === 0 || tempVal.indexOf('mul') === 0 || tempVal.indexOf('minus') === 0 || tempVal.indexOf('plus') === 0 || tempVal.indexOf('equal') === 0){
+            tempVal = "";
+          }
           showData(tempVal);
           break;
       }
     }
   });
 
-  // Displays symbols on the calc screen
+  // Displays symbols on the screen
   function showData(data){
     if(data === ""){
       resultObj.innerHTML = "0";
