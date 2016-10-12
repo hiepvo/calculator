e8297a0dd39566106a8c703d8f3d033daac125e9
hiep.vo <vanhiep1086@gmail.com>
2016-10-11 20:16:02 -0500

    add javascript & update design

--- main.js
+++ main.js
@@ -2,62 +2,83 @@
  * Created by hiepvo on 10/9/16.
  */
 (function(){
-  var tempVal   = "";
+  var tempVal   = [];
   var buttonId  = "";
+  var buttonVal = "";
   var ul        = document.getElementById("keys");
   var resultObj = document.getElementsByClassName("result")[0];
   ul.addEventListener("click", function(e){
     if(e.target.tagName = "li"){
-      buttonId = e.target.id;
+      buttonId  = e.target.id;
+      buttonVal = e.target.innerText;
       switch(buttonId){
         case "clear":
-          tempVal = "";
-          showData(tempVal);
+          tempVal = [];
+          showData(tempVal.join(''));
           break;
         case "del":
           if(tempVal.length > 1){
-            if(tempVal.length === 8 && tempVal.indexOf("&minus;") === 0){ // tempVal.length = 8 = &minus;8  = -8
-              tempVal = "";
+            //check if there is minus sign before number then set it to 0
+            if(tempVal.length === 2 && tempVal[0] === "&minus;"){
+              tempVal = [];
             }
             else
-              tempVal = tempVal.slice(0, -1);
+              tempVal.splice(-1, 1); //remove last item from an array
           }
           else{
-            tempVal = "";
+            tempVal = [];
           }
-          showData(tempVal);
+          showData(tempVal.join(''));
           break;
         case "neg":
-          if(tempVal !== ""){
-            if(tempVal.indexOf("&minus;") === -1){
-              tempVal = "&minus;" + tempVal;
-
+          if(tempVal.length !== 0){
+            if(tempVal[0] !== "&minus;"){ // check if first item is minus sign
+              tempVal.unshift("&minus;"); // if no add it
             }
             else{
-              tempVal = tempVal.replace("&minus;", "");
+              tempVal.shift(); // remove minus sign
             }
           }
-          showData(tempVal);
+          showData(tempVal.join(''));
+          break;
+        case "equal":
+          showData(eval(tempVal.join('')));
           break;
         default:
-          tempVal += buttonId;
-          if(tempVal.indexOf('div') === 0 || tempVal.indexOf('mul') === 0 || tempVal.indexOf('minus') === 0 || tempVal.indexOf('plus') === 0 || tempVal.indexOf('equal') === 0){
-            tempVal = "";
+          tempVal.push(buttonVal);
+          //check if string start with one of these operators (+, - , * , /, =)
+          if(tempVal.length === 1 && (buttonId === 'divide' || buttonId === 'times' || buttonId === 'minus' || buttonId === 'plus' || buttonId === 'equal')){
+            tempVal = [];
           }
-          showData(tempVal);
+
+          /*
+           //check if two operator after each other
+           if(tempVal.length > 1){
+           var i = 0;
+           for(i; i < tempVal.length; i++){
+           if(operators.indexOf(tempVal[i]) > -1){
+           alert('')
+           if(operators.indexOf(tempVal[i]) === operators.indexOf(tempVal[i + 1])){
+           tempVal = "";
+           }
+           }
+           }
+           }*/
+          showData(tempVal.join(''));
           break;
       }
     }
   });
 
-  // Displays symbols on the screen
+// Displays symbols on the screen
   function showData(data){
-    if(data === ""){
+    if(data.length === 0){
       resultObj.innerHTML = "0";
     } else{
       resultObj.innerHTML = data;
     }
   }
-})();
+})
+();
 
 
